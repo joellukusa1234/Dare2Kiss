e197de758aa44498e888cdeca5c03dd475e05e64
joellukusa1234 <joellukusa1234@gmail.com>
2026-06-20 16:05:32 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 ASGI_APPLICATION = 'config.asgi.application'
 
