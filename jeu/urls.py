67cd57c0275b2291072f9308afaef532dfdb15e5
joellukusa1234 <joellukusa1234@gmail.com>
2026-06-20 16:52:31 +0100

    Update urls.py

--- jeu/urls.py
+++ jeu/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.salon_jeu, {'room_name': 'principal'}, name='salon_defaut'),
     path('salon/<str:room_name>/', views.salon_jeu, name='salon_jeu'),
 ]
